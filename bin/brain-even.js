505e81b6b7917f05f14c35f14a9b113c15d85ee1
messkos <anon558@gmail.com>
2022-10-10 17:26:38 +0300

    Add functionality to brain-even
    Now it's working

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -2,4 +2,33 @@
 
 import readlineSync from 'readline-sync';
 
-console.log('asdsdfsdf')
+const getRandomInt = () => {
+    const randomInt = Math.floor(Math.random() * 101);
+    return randomInt;
+}
+
+const getCorrectAnswer = (num) => {
+    if (num % 2 === 0) {
+        return 'yes';
+    } else {
+        return 'no';
+    }
+}
+const brainEven = () => {
+    console.log('Welcome to the Brain Games!');
+    const name = readlineSync.question('May I have your name?: ');
+    console.log(`Hello, ${name}!`);
+    console.log('Answer "yes" if the number is even, otherwise answer "no".');
+    for (let i = 0; i < 3; i += 1) {
+        const randomNumber = getRandomInt();
+        const answer = readlineSync.question(`Question: ${randomNumber}\nYour answer:`);
+        const correctAnswer = getCorrectAnswer(randomNumber);
+        if (answer === correctAnswer) {
+            console.log('Correct!');
+        } else {
+            return console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${name}!`);
+        }
+    }
+    console.log(`Congratulations, ${name}!`);
+}
+brainEven();
