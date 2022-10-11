4fee5d9b88c6f3bb79245b2500c478281cc381e3
messkos <anon558@gmail.com>
2022-10-11 12:21:21 +0300

    Add /src/index.js
    index.js now stores general functions

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import readlineSync from 'readline-sync';
+
+const greetingUser = () => {
+    console.log('Welcome to the Brain Games!');
+    const name = readlineSync.question('May I have your name?: ');
+    console.log(`Hello, ${name}!`);
+    return name
+}
+const getRandomInt = () => {
+    const randomInt = Math.floor(Math.random() * 101);
+    return randomInt;
+}
+
+const getCorrectAnswerEven = (num) => {
+    if (num % 2 === 0) {
+        return 'yes';
+    } else {
+        return 'no';
+    }
+}
+const askQuestion = (terms) => {
+    return readlineSync.question(`Question: ${terms}\nYour answer:`);
+}
+
+export {greetingUser, getRandomInt, getCorrectAnswerEven, askQuestion}
